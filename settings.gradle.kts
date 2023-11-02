aa101e7e888c32b5061cefdcbc6eac0f7e864d47
Daimhim <daimhim@163.com>
2023-11-02 14:43:25 +0800

    1. 干脆、直接、操作service，不做任何中转

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -12,12 +12,6 @@ pluginManagement {
         maven("https://jitpack.io")
         maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
         maven("https://maven.pkg.jetbrains.space/kotlin/p/kotlin/bootstrap/")
-        maven("https://devrepo.devcloud.cn-east-3.huaweicloud.com/artgalaxy/cn-east-3_74e793c6bfdf4b258360413fd0eba4c7_maven_2_2/"){
-            credentials {
-                username = "cn-east-3_74e793c6bfdf4b258360413fd0eba4c7_32540a29aa2e4157a4d3a0233c5b895b"
-                password = "6l^EL-1aVx"
-            }
-        }
     }
 
     plugins {
